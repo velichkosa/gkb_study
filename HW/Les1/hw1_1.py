7f0b0e40b66938dd5f702c747feb0ab1232a6d3d
Sergey <velichko.sergey.a@gmai.com>
2020-11-29 12:48:22 +0500

    hw1

---
+++ HW/Les1/hw1_1.py
@@ -0,0 +1,15 @@
+a = 1  #int
+b = 1.5  #float
+c = "stroka"  #string
+d = True  #bool
+e = { 1, 2, 3}  #set
+f = ( 1, 2, 3)  #tuple
+g = [ 1, 2, 3]  #list
+
+print("a =", a, type(a),"\n",
+      "b =", b, type(b),"\n",
+      "c =", c, type(c),"\n",
+      "d =", d, type(d),"\n",
+      "e =", e, type(e),"\n",
+      "f =", f, type(f),"\n",
+      "g =", g, type(g),"\n")
