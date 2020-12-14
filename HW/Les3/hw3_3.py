8bce63cf92b29ccad4df05d3682ff7b3dfee745e
Sergey <velichko.sergey.a@gmai.com>
2020-12-14 23:09:51 +0500

    HomeWORK4

--- HW/Les3/hw3_3.py
+++ HW/Les3/hw3_3.py
@@ -1,24 +1,11 @@
-# 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
-# и возвращает сумму наибольших двух аргументов.
+""" 3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
+и возвращает сумму наибольших двух аргументов."""
 
-def my_func(arg1= int(input('Type value for argument #1: ')),
-            arg2= int(input('Type value for argument #2: ')),
-            arg3= int(input('Type value for argument #3: '))):
+arg = input('Type value for argument: ')
+arg = arg.split()
+arg = [int(l) for l in arg]
 
-    l= [arg1, arg2, arg3]
-    big1= int()
-    big2= int()
+def my_func(arg1):
+    return sum(arg1) - min(arg1)
 
-    for i in l:
-        if i > big1:
-            big1= i
-    l.pop(l.index(big1))
-
-    for i in l:
-        if i > big2:
-            big2= i
-    l.pop(l.index(big2))
-
-    return big1+big2
-
-print(my_func())
+print(my_func(arg))
